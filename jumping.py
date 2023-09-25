680c6610b922f4e43d580f466ee6d6084f1db003
9885935652 <gowthamc121@gmail.com>
2023-09-25 20:39:09 +0530

    python

---
+++ jumping.py
@@ -0,0 +1,20 @@
+'''
+pass
+break
+continue
+
+'''
+
+a="gowtham"
+for i in a:
+    if i=='a':
+        break
+    print(i)
+
+print("----------------------------------------------------")
+
+
+for i in a:
+    if i=='t':
+        continue
+    print(i)
